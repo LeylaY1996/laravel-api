46d31ae645c8aa24da1746489fd0f20970b323f4
Leylayil <leylay1996@gmail.com>
2020-11-10 18:43:58 +0300

    rules added

--- app/Http/Controllers/Country/CountryController.php
+++ app/Http/Controllers/Country/CountryController.php
@@ -2,9 +2,10 @@
 
 namespace App\Http\Controllers\Country;
 
-use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 use App\Models\CountryModel;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Validator;
 
 class CountryController extends Controller
 {
@@ -17,7 +18,7 @@ class CountryController extends Controller
     {
         $country = CountryModel::find($id);
         if (is_null($country)) {
-            return response()->json("Böyle bir ülke yok", 404);
+            return response()->json(["message"=>"Böyle bir ülke yok"], 404);
         }
         return response()->json($country, 200);
     }
@@ -26,6 +27,15 @@ class CountryController extends Controller
     /* 201 create save success code */
     public function countrySave(Request $request)
     {
+        $rules = [
+            'name' => 'required|min:3',
+            'iso' => 'required|min:2|max:5'
+        ];
+        $validator = Validator::make($request->all(), $rules);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 400);
+        }
         $country = CountryModel::create($request->all());
         return response()->json($country, 201);
     }
@@ -34,7 +44,7 @@ class CountryController extends Controller
     {
         $country = CountryModel::find($id);
         if (is_null($country)) {
-            return response()->json("Böyle bir ülke yok", 404);
+            return response()->json(["message"=>"Böyle bir ülke yok"], 404);
         }
         $country->update($request->all());
         return response()->json($country, 200);
@@ -45,7 +55,7 @@ class CountryController extends Controller
     {
         $country = CountryModel::find($id);
         if (is_null($country)) {
-            return response()->json("Böyle bir ülke yok", 404);
+            return response()->json(["message"=>"Böyle bir ülke yok"], 404);
         }
         $country->delete();
         return response()->json(null, 204);
